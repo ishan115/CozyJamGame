00d3453f2c7b7be91a7b318627118c820ee952e9
AYogore <58088654+AYogore@users.noreply.github.com>
2019-11-22 14:41:00 -0600

    Add files via upload

---
+++ Assets/Shooting.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shooting : MonoBehaviour
+{
+    public Transform firePoint;
+    public GameObject BulletPreFab;
+
+    public float BulletForce = 20f;
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            Shoot();
+        }
+    }
+
+    void Shoot()
+    {
+        GameObject Bullet = Instantiate(BulletPreFab, firePoint.position, firePoint.rotation);
+        Rigidbody2D rb = Bullet.GetComponent<Rigidbody2D>();
+        rb.AddForce(firePoint.up * BulletForce, ForceMode2D.Impulse);
+    }
+}
