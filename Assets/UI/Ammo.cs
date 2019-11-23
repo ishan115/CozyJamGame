5958eb594a680fdc68d4ae39507a80b5b5a4055c
Ishan Dutta <ishan.dutta2006@yahoo.co.in>
2019-11-23 14:53:16 -0600

    Added Ammo count and script + Updated Git ignore + Removed Herobrine

---
+++ Assets/UI/Ammo.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Ammo : MonoBehaviour
+{
+    public int ammo;
+    public int ammoTotal;
+
+    public Image[] snowballs;
+    public Sprite fullAmmo;
+    public Sprite noAmmo;
+        
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (ammo > ammoTotal)
+        {
+            ammo = ammoTotal;
+        }
+
+        for (int i = 0; i < snowballs.Length; i++)
+        {
+            if (i<ammo)
+            {
+                snowballs[i].sprite = fullAmmo;
+            }
+            else
+            {
+                snowballs[i].sprite = noAmmo;
+            }
+            if (i < ammoTotal)
+            {
+                snowballs[i].enabled = true;
+            }
+            else
+            {
+                snowballs[i].enabled = false;
+            }
+        }
+    }
+}
