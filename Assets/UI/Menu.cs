7441026a9ede7a1dfad0f923347b313ade97d40c
Ishan Dutta <ishan.dutta2006@yahoo.co.in>
2019-11-23 11:09:42 -0600

    Update Menu(UI) script

--- Assets/UI/Menu.cs
+++ Assets/UI/Menu.cs
@@ -1,18 +1,40 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class Menu : MonoBehaviour
 {
+    public GameObject mainMenuCanvas;
+    public GameObject creditsCanvas;
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        mainMenuCanvas.SetActive(true);    
+    }
+    
+    public void LoadGame()
+    {
+        SceneManager.LoadScene("MainGame");
     }
 
-    // Update is called once per frame
-    void Update()
+    public void LoadMainMenu()
     {
-        
+        mainMenuCanvas.SetActive(true);
+        creditsCanvas.SetActive(false);
+    }
+
+    public void LoadCredits()
+    {
+        mainMenuCanvas.SetActive(false);
+        creditsCanvas.SetActive(true);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
     }
 }
